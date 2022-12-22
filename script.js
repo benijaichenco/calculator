c3fa166875615126288afca39c35abe3f2294876
Beni <benjaminjaichenco@gmail.com>
2022-12-22 19:54:53 +0200

    create basic math functions

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 const clear = document.querySelector('.clear');
 const deleteLastNum = document.querySelector('.delete');
+//
+
+
+//create basic math functions
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+//
+
