259c512a4db590e8d87ee0a22bb34590dd1fa352
Beni <benjaminjaichenco@gmail.com>
2022-12-22 19:50:44 +0200

    create variables

---
+++ script.js
@@ -0,0 +1,7 @@
+//create variables
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const clear = document.querySelector('.clear');
+const deleteLastNum = document.querySelector('.delete');
